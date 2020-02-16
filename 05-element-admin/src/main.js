b9f7261f12605515dfaf40bae8e41d3c8520bfd9
0xbul1 <diaoszhao@gmail.com>
2020-02-17 00:04:33 +0800

    完成一个简单的博客文章增删改查

--- 05-element-admin/src/main.js
+++ 05-element-admin/src/main.js
@@ -1,11 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './plugins/element.js';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import axios from 'axios';
+
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:3000/api',
+});
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
